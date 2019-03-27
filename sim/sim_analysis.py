e229a7cc221144fd3e5f5a5f2f73c768b7770445
Ruchir Aggrawal <e0216119@u.nus.edu>
2019-03-27 12:30:51 -0400

    add script to analyze simulations Add comments to run.py

--- sim/sim_analysis.py
+++ sim/sim_analysis.py
@@ -17,17 +17,49 @@ def form_value_dataframe(env_name, file_name):
     ev_col = get_stat_col(file_name, 'ev')
     opt_dyn_loss_col = get_stat_col(file_name, 'opt_dyn_loss')
     recent_best_ext_ret_col = get_stat_col(file_name, 'recent_best_ext_ret')
+    best_ext_ret_col = get_stat_col(file_name, 'best_ext_ret')
 
+    plt.cla()
     ax = advmean_col.plot(title= env_name + ': Mean of Agent\'s advantage')
-    plt.show()
+    ax.set_xlabel('Number of Updates')
+    ax.set_ylabel('Mean Advantage')
+    mat_fig = ax.get_figure()
+    mat_fig.savefig('plots/' + env_name + '_advmean.png', bbox_inches='tight')
+
+    plt.cla()  # Clear axis
     ax_1 = advstd_col.plot(title=env_name + ': Standard Deviation of Agent\'s advantage')
-    plt.show()
+    ax_1.set_xlabel('Number of Updates')
+    ax_1.set_ylabel('S.D. of Advantage')
+    mat_fig = ax_1.get_figure()
+    mat_fig.savefig('plots/' + env_name + '_advstd.png', bbox_inches='tight')
+
+    plt.cla()  # Clear axis
     ax_2 = ev_col.plot(title=env_name + ': Explained variance of Agent')
-    plt.show()
+    ax_2.set_xlabel('Number of Updates')
+    ax_2.set_ylabel('EV')
+    mat_fig = ax_2.get_figure()
+    mat_fig.savefig('plots/' + env_name + '_ev.png', bbox_inches='tight')
+
+    plt.cla()  # Clear axis
     ax_3 = opt_dyn_loss_col.plot(title=env_name + ': Optimum Dynamics Loss for Agent')
-    plt.show()
+    ax_3.set_xlabel('Number of Updates')
+    ax_3.set_ylabel('ODL')
+    mat_fig = ax_3.get_figure()
+    mat_fig.savefig('plots/' + env_name + '_opt_dyn_loss.png', bbox_inches='tight')
+
+    plt.cla()  # Clear axis
     ax_4 = recent_best_ext_ret_col.plot(title=env_name + ': Best recent Extrinsic Reward Agent received')
-    plt.show()
+    ax_4.set_xlabel('Best Recent ER')
+    ax_4.set_ylabel('')
+    mat_fig = ax_4.get_figure()
+    mat_fig.savefig('plots/' + env_name + '_recent_best_ext_ret.png', bbox_inches='tight')
+
+    plt.cla()  # Clear axis
+    ax_5 = best_ext_ret_col.plot(title=env_name + ': Best Extrinsic Reward Agent received')
+    ax_5.set_xlabel('Number of Updates')
+    ax_5.set_ylabel('Best ER')
+    mat_fig = ax_5.get_figure()
+    mat_fig.savefig('plots/' + env_name + '_best_ext_ret.png', bbox_inches='tight')
     return
 
 
